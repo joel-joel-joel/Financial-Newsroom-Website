74e970231be84452db1965371acb25e635581041
joel-joel-joel <leojongenli@gmail.com>
2025-11-08 22:18:48 +0000

    Change AI model to flash

--- api/chat.js
+++ api/chat.js
@@ -2,7 +2,8 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash-latest' });
+
 
 
 export default async function handler(req, res) {
