545e83c8136ba6c8f567ac2a69d5aca2120beadb
joel-joel-joel <leojongenli@gmail.com>
2025-11-08 22:07:16 +0000

    Change AI model to gemini pro

--- api/chat.js
+++ api/chat.js
@@ -2,7 +2,8 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+
 
 export default async function handler(req, res) {
   // CORS for local & prod
